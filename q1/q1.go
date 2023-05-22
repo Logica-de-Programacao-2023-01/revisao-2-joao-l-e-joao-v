33ba5a070ed2ef9d0b430dcb0cf5b514af184458
joaoplima04 <126472676+joaoplima04@users.noreply.github.com>
2023-05-22 08:35:28 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -15,12 +15,19 @@ type Student struct {
 }
 
 func MergeStudentData(studentData1 map[string]Student, studentData2 map[string]Student) map[string]Student {
-	juncao := make(map[string]Student)
+		juncao := make(map[string]Student)
 	for chave, valor := range studentData1 {
 		juncao[chave] = valor
 	}
 	for chave, valor := range studentData2 {
-		juncao[chave] = valor
+		if existingStudent, ok := juncao[chave]; ok {
+			for subject, grade := range valor.Subjects {
+				existingStudent.Subjects[subject] = grade
+			}
+		} else {
+			juncao[chave] = valor
+
+		}
 	}
 	return juncao
 }
